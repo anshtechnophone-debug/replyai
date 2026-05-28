1772285a79d0bbf31b57daa30a6b0770890a845a
anshtechnophone-debug <anshtechnophone@gmail.com>
2026-05-28 11:54:18 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,9 @@
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
     }
@@ -15,12 +20,14 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
