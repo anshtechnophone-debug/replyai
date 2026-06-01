cadd76973250daf74bc75361421cbcee4463585c
anshtechnophone-debug <anshtechnophone@gmail.com>
2026-06-01 20:50:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart' hide AppState; // 👈 add "hide AppState"
+import 'package:google_mobile_ads/google_mobile_ads.dart' hide AppState;
 import 'package:provider/provider.dart';
 import 'src/app.dart';
 import 'src/services/app_state.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,  // ✅ explicit options
+  );
   await MobileAds.instance.initialize();
   runApp(
     ChangeNotifierProvider(
