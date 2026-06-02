378eca2b5a832361fb989ca7c8e7175f5996d67a
anshtechnophone-debug <anshtechnophone@gmail.com>
2026-06-02 07:39:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,  // ✅ explicit options
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   await MobileAds.instance.initialize();
   runApp(
